b69a4d5b8279ccdede5ea743cf2fb235b15aa40e
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-27 12:16:55 -0500

    add comments to understand backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const express = require("express");
 const app = express();
+//3-4: creates a new Express server
 const users = require("./routes/api/users");
 const friendrequests = require("./routes/api/friend-requests");
 const events = require("./routes/api/events");
@@ -34,5 +35,6 @@ app.use("/api/users", users);
 app.use("/api/friend-requests", friendrequests);
 app.use("/api/events", events);
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+//37-38: Before we can run the server, we need to tell our app which port to run on
+//deploying our app to Heroku requires run our server on process.env.PORT
